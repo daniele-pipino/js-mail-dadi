8adc9037ae9f1464fd80948cf1754d2466fc4955
daniele-pipino <Danielemacpipino@icloud.com>
2021-07-02 16:10:01 +0200

    Richiesta mail all'utente

--- js/main.js
+++ js/main.js
@@ -1,1 +1,27 @@
-console.log('js ok');
+// ----------------------------------Traccia-----------------------------------------
+
+// Mail
+// Chiedi all’utente la sua email,
+// controlla che sia nella lista di chi può accedere,
+// stampa un messaggio appropriato sull’esito del controllo.
+
+
+// Gioco dei dadi
+// Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+// Stabilire il vincitore, in base a chi fa il punteggio più alto e stampare il risultato.
+// Prima di partire a scrivere codice poniamoci qualche domanda:
+// Che ci sia un array da qualche parte?
+// Se dobbiamo confrontare qualcosa che "cosa" ci serve?
+
+
+// ----------------------------------Svolgimento Mail-----------------------------------------
+
+
+// chiediamo all'utente la sua mail
+
+var mailrequest = prompt('Inserisci la tua mail');
+console.log('Mail: ', mailrequest);
+
+// verificare l'esistenza della  mail
+
+
